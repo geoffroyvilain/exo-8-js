fd868b6df826d6885b372bf43356f0a51567a8fe
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-01-22 16:09:47 +0100

    exo fini

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+function getcheck() {
+  var age = document.getElementById('age').value;
+  if (age>=18) {
+  alert ('Vous êtes majeur');
+  }else{
+    alert ('Vous êtes mineur');
+
+}
+}
+
+
+
+
+
+
+/*function getNumber() {
+var pointure = document.getElementById('pointure').value;
+var annee = document.getElementById('annee').value;
+if (isNaN(pointure)==false && isNaN(annee)==false) {
+              alert(((((pointure*2)+5)*50)-1985)+1766);
+            }else{
+              alert('Merci de renseigner les champs!');
+}
+}
+*/
