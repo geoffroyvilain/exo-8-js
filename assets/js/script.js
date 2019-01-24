3107db76d8254acc8173c4abf38f8c9198dac460
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-01-24 16:15:47 +0100

    exo corrige

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,16 @@
-
 function getcheck() {
   var age = document.getElementById('age').value;
-  if (age>=18) {
-  alert ('Vous êtes majeur');
-  }else{
-    alert ('Vous êtes mineur');
-
+  if(!isNaN(age)){   //si age est un nombre
+    if( age< 114 && age > 0){   // et si l age est compris entre 0 et 114
+      if(age >= 18){            // et si l'age est superieur a 18
+        alert('Vous êtes majeur');   // alors afficher vous etes majeur
+      }else{                          // sinon
+        alert('Vous êtes mineur');  // alors afficher vous etes mineur
+      }
+    }else{                          //sinon
+      alert('Age non valide');      //afficher age non valide
+    }
+  }else{                            //sinon
+    alert('merci de saisir en chiffres');   // afficher merci .......
+  }
 }
-}
-
-
-
-
-
-
-/*function getNumber() {
-var pointure = document.getElementById('pointure').value;
-var annee = document.getElementById('annee').value;
-if (isNaN(pointure)==false && isNaN(annee)==false) {
-              alert(((((pointure*2)+5)*50)-1985)+1766);
-            }else{
-              alert('Merci de renseigner les champs!');
-}
-}
-*/
